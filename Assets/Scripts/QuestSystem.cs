b0e4e98be26f9d8295a4072585fea3ced14a0e52
Yorgi11 <michael.giorgi@ontariotechu.net>
2025-02-13 01:04:13 -0500

    Quest UI prototype

--- Assets/Scripts/QuestSystem.cs
+++ Assets/Scripts/QuestSystem.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 using TMPro;
 public class QuestSystem : MonoBehaviour
 {
+    [SerializeField] private int m_maxActiveQuests;
+    [SerializeField] private TextMeshProUGUI m_questText;
     [SerializeField] private TextMeshProUGUI m_questUITextPrefab;
     [SerializeField] private Transform m_questUIparent;
     [SerializeField] private Quest[] m_questArchive;
-    private List<Quest> m_activeQuests;
-    private List<TextMeshProUGUI> m_questUITexts;
+    private List<Quest> m_activeQuests = new();
+    private List<TextMeshProUGUI> m_questUITexts = new();
     [System.Serializable]
     public struct Quest
     {
@@ -21,8 +23,13 @@ public class QuestSystem : MonoBehaviour
         public bool m_isCompleted;
         public string m_taskDescription;
     }
+    private void Start()
+    {
+        UpdateQuestUI();
+    }
     private void UpdateQuestUI()
     {
+        if (m_activeQuests.Count > m_maxActiveQuests) return;
         int c = m_questUIparent.childCount;
         if (c < m_activeQuests.Count)
         {
@@ -35,6 +42,7 @@ public class QuestSystem : MonoBehaviour
         {
             m_questUITexts[i].text = m_activeQuests[i].m_questDescription;
         }
+        m_questText.text = $"Quests                {m_activeQuests.Count}/5";
     }
     public void AddQuest(Quest q)
     {
@@ -43,11 +51,25 @@ public class QuestSystem : MonoBehaviour
     }
     public void RemoveQuest(Quest q)
     {
-        m_activeQuests.Remove(q);
+        int i = m_activeQuests.IndexOf(q);
+        if (i < 0)
+        {
+            Debug.LogWarning("Attempted to remove a quest that doesn't exist in the active list.");
+            return;
+        }
+        if (i < m_questUITexts.Count)
+        {
+            Destroy(m_questUITexts[i].gameObject);
+            m_questUITexts.RemoveAt(i);
+        }
+        m_activeQuests.RemoveAt(i);
         UpdateQuestUI();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.L)) AddQuest(m_questArchive[0]);
+        if (Input.GetKeyDown(KeyCode.P)) AddQuest(m_questArchive[1]);
+        if (Input.GetKeyDown(KeyCode.K)) RemoveQuest(m_questArchive[0]);
+        if (Input.GetKeyDown(KeyCode.O)) RemoveQuest(m_questArchive[1]);
     }
 }
