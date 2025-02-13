73efdffff83627a8f6f68b761cdffe2fc3df4ed3
Yorgi11 <michael.giorgi@ontariotechu.net>
2025-02-13 00:11:23 -0500

    no clue

--- Assets/Scripts/QuestSystem.cs
+++ Assets/Scripts/QuestSystem.cs
@@ -1,20 +1,53 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 public class QuestSystem : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI m_questUITextPrefab;
+    [SerializeField] private Transform m_questUIparent;
     [SerializeField] private Quest[] m_questArchive;
     private List<Quest> m_activeQuests;
+    private List<TextMeshProUGUI> m_questUITexts;
     [System.Serializable]
-    private struct Quest
+    public struct Quest
     {
         public bool m_isCompleted;
         public string m_questDescription;
         public QuestTask[] m_tasks;
     }
     [System.Serializable]
-    private struct QuestTask
+    public struct QuestTask
     {
         public bool m_isCompleted;
         public string m_taskDescription;
+    }
+    private void UpdateQuestUI()
+    {
+        int c = m_questUIparent.childCount;
+        if (c < m_activeQuests.Count)
+        {
+            for (int i = 0; i < m_activeQuests.Count - c; i++)
+            {
+                m_questUITexts.Add(Instantiate(m_questUITextPrefab, m_questUIparent));
+            }
+        }
+        for (int i = 0; i < m_activeQuests.Count; i++)
+        {
+            m_questUITexts[i].text = m_activeQuests[i].m_questDescription;
+        }
+    }
+    public void AddQuest(Quest q)
+    {
+        m_activeQuests.Add(q);
+        UpdateQuestUI();
+    }
+    public void RemoveQuest(Quest q)
+    {
+        m_activeQuests.Remove(q);
+        UpdateQuestUI();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L)) AddQuest(m_questArchive[0]);
     }
 }
